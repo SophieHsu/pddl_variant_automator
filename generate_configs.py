4f2191e6d34d5912101499efcf02d8bc0d8e76dd
misoshiruseijin <ayano.hiranaka@gmail.com>
2025-09-23 23:53:56 -0700

    add config generation from metaconfig

---
+++ generate_configs.py
@@ -0,0 +1,213 @@
+
+import argparse
+import json
+import itertools
+from collections import defaultdict
+
+from pddl_knowledge.utils.pddl_chunk_utils import chunk_pddl
+
+def _get_all_action_names(domain_chunks):
+    all_action_names = []
+    for chunk in domain_chunks:
+        if chunk["chunk_type"] == "action":
+            # print(chunk)
+            all_action_names.append(chunk["chunk_name"])
+
+    return all_action_names
+
+def _get_true_object_types(problem_chunks):
+    chunk_types = [chunk["chunk_type"] for chunk in problem_chunks]
+    objects_block = problem_chunks[chunk_types.index("objects")]
+    lines = [line.strip() for line in objects_block["raw_text"].splitlines()]
+    true_obj_types = {}
+    for line in lines:
+        if "-" in line:
+            objects, type = line.split("-")
+            objects = objects.split()
+            for obj in objects:
+                true_obj_types[obj.strip()] = type.strip()
+
+    return true_obj_types
+
+def _get_obj_to_type_candidates(type_to_obj_candidates):
+    out = defaultdict(list)
+    for type, objs in type_to_obj_candidates.items():
+        for obj in objs:
+            out[obj].append(type)
+
+    return out
+
+def main(args):
+    
+    # load meta config
+    with open(args.metaconfig, "r") as f:
+        file = f.read()
+    metaconf = json.loads(file)
+
+    # load pddls and get chunks
+    domain_chunks = chunk_pddl(args.domain)
+    problem_chunks = chunk_pddl(args.problem)
+    all_action_names = _get_all_action_names(domain_chunks)
+    task_name = metaconf["task_name"]
+
+    variants = []
+
+    """ 
+    Cost Spike Variants 
+    """
+    if metaconf["use_cost_spikes"]:
+
+        cost_spike_actions = metaconf["actions_for_cost_spike"]
+        cost_spike_values = metaconf["cost_spike_values"]
+
+        # if all
+        if cost_spike_actions == "all":
+            cost_spike_actions = all_action_names
+
+        # generate entries
+        for action in cost_spike_actions:
+            for spike_val in cost_spike_values:
+                variants.append({
+                    "name": f"{task_name}_spike_{action}_{spike_val}",
+                    "transforms": [
+                        {"op": "add_cost_spike", "action_substr": action, "K": spike_val},
+                        {"op": "ensure_metric"},
+                    ],
+                })
+    
+    """
+    Budget Decrease Variants
+    """
+    if metaconf["use_budget_decrease"]:
+        budget_decrease_actions = metaconf["actions_for_budget_decrease"]
+        budget_initial_values = metaconf["budget_initial_values"]
+        budget_decrease_values = metaconf["budget_decrease_values"]
+        budget_decrease_special_action_sets = metaconf["budget_actions_special_action_sets"]
+
+        """ Single action cases """
+        if budget_decrease_actions == "all":
+            budget_decrease_actions = all_action_names
+        
+        for action in budget_decrease_actions:
+            for init_val in budget_initial_values:
+                for decrease_val in budget_decrease_values:
+                    variants.append({
+                        "name": f"{task_name}_budget_{action}",
+                        "transforms": [
+                            {"op": "ensure_budget", "name": "budget", "init": init_val},
+                            {"op": "decrease_budget_on", "action_substr": action, "name": "budget", "amount": decrease_val},
+                            {"op": "ensure_metric"}
+                        ],
+                    })
+
+        """ Special action sets """
+        for name, data in budget_decrease_special_action_sets.items():
+            transforms = [
+                    {"op": "ensure_budget", "name": "budget", "init": init_val},
+            ]
+            for act, val in zip(data["actions"], data["decrease"]):
+                transforms.append(
+                    {"op": "decrease_budget_on", "action_substr": act, "name": "budget", "amount": val},
+                )
+            transforms.append({"op": "ensure_metric"})
+            variants.append({
+                "name": f"{task_name}_budget_{name}",
+                "transforms": transforms,
+            })
+            
+    """
+    Remove Effects Variants (remove all occurrences of an effect)
+    """
+    if metaconf["use_remove_effects_all"]:
+        removable_effects = metaconf["removable_effects"]
+        for effect in removable_effects:
+            variants.append({
+                "name": f"{task_name}_hazoff_all_{effect}",
+                "transforms": [
+                    {"op": "remove_effect_predicates", "predicates": [effect]},
+                    {"op": "ensure_metric"},
+                ]
+            })
+
+    """
+    TODO
+    Remove Effects Variants (remove effect from single action)
+    """
+    if metaconf["use_remove_effects_single"]:
+        removable_effects = metaconf["removable_effects"]
+        for effect in removable_effects:
+            for action in all_action_names:
+                variants.append({
+                    "name": f"{task_name}_hazoff_{effect}_{action}",
+                    "transforms": [
+                        {"op": "remove_effect_from_single_action", "predicates": [effect], "action_substr": action},
+                        {"op": "ensure_metric"},
+                    ]
+                })
+
+    """
+    Type Swap Variants
+    """
+    if metaconf["use_type_swaps"]:
+        type_to_obj_candidates = metaconf["object_type_to_compatible_objects"]
+        
+        # get object types defined in the problem file
+        true_obj_types = _get_true_object_types(problem_chunks)
+        obj_to_type_candidates = _get_obj_to_type_candidates(type_to_obj_candidates)
+        swapped_pairs = []
+
+        for obj, types in obj_to_type_candidates.items():
+            true_type = true_obj_types[obj]
+            ##### CASE 1: reassign SINGLE object to another type #####
+            for type in types:
+                if not type == true_type:
+                    variants.append({
+                        "name": f"{task_name}_type_{obj}_from_{true_type}_to_{type}",
+                        "transforms": [
+                            {"op": "swap_object_types", "renames": {obj: type}},
+                            {"op": "ensure_metric"}
+                        ]
+                    })
+            ##### CASE 2: swapping two object types #####
+            type_pairs = itertools.combinations(types, 2)
+            all_objs = obj_to_type_candidates.keys()
+            for pair in type_pairs:
+                for pair_candidate in all_objs:
+                    if pair_candidate == obj:
+                        continue
+                    # skip if already swapped
+                    if set(pair) in swapped_pairs:
+                        continue
+                    # conditions for legal swapping
+                    candidate_true_type = true_obj_types[pair_candidate]
+                    cond1 = true_type in obj_to_type_candidates[pair_candidate]
+                    cond2 = candidate_true_type in obj_to_type_candidates[obj]
+                    if cond1 and cond2:
+                        variants.append({
+                            "name": f"{task_name}_type_swap_{obj}_and_{pair_candidate}",
+                            "transforms": [
+                                {"op": "swap_object_types", "renames": {obj: candidate_true_type}},
+                                {"op": "swap_object_types", "renames": {pair_candidate: true_type}},
+                                {"op": "ensure_metric"}
+                            ]
+                        })
+                        swapped_pairs.append(set(pair))
+
+    """
+    Save generated config
+    """
+    config = {"variants": variants}
+    options = {"indent": 4}
+
+    with open(args.savepath, "w") as f:
+        json.dump(config, f, **options)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--metaconfig", type=str, help="path to meta config json", default="/home/ayanoh/pddl_variant_automator/configs/metaconfigs/breakfast_meta.json")
+    parser.add_argument("--domain", type=str, help="path to pddl domain file", default="/home/ayanoh/pddl_variant_automator/task_definitions/gt/breakfast_domain.pddl")
+    parser.add_argument("--problem", type=str, help="path to pddl problem file", default="/home/ayanoh/pddl_variant_automator/task_definitions/gt/breakfast_problem.pddl")
+    parser.add_argument("--savepath", type=str, help="path to save generated config json", default="test.json")
+    args = parser.parse_args()
+
+    main(args)
--- generate_variants.py
+++ generate_variants.py
@@ -12,7 +12,7 @@ Usage:
 import argparse, json, os, sys, copy
 from typing import Dict, Any, List
 from sexpr import parse, dumps
-from transforms import add_cost_spike, remove_effect_predicates, swap_object_types, ensure_metric_has_total_cost, neutralize_damage, scale_duration, ensure_numeric_fluent, ensure_init_numeric, decrease_fluent_on
+from transforms import add_cost_spike, remove_effect_predicates, swap_object_types, ensure_metric_has_total_cost, neutralize_damage, scale_duration, ensure_numeric_fluent, ensure_init_numeric, decrease_fluent_on, remove_effect_from_single_action
 
 def load_text(path): 
     with open(path, 'r', encoding='utf-8') as f: 
@@ -31,21 +31,24 @@ def apply_recipe(domain_ast, problem_ast, recipe, verbose=False):
             ok = add_cost_spike(domain_ast, step.get("action_substr",""), int(step.get("K",100000)), bool(step.get("at_end", False)))
         elif op == "remove_effect_predicates":
             ok = remove_effect_predicates(domain_ast, step.get("predicates", [])) > 0
+        elif op == "remove_effect_from_single_action":
+            ok = remove_effect_from_single_action(domain_ast, step.get("predicates", []), step.get("action_substr","")) > 0
         # elif op == "neutralize_damage":
         #     ok = neutralize_damage(domain_ast, step.get("extra_predicates")) > 0
         # elif op == "scale_duration":
         #     ok = scale_duration(domain_ast, step.get("action_substr",""), float(step.get("factor",2.0)), step.get("absolute"))
         elif op == "swap_object_types":
             ok = swap_object_types(problem_ast, step.get("renames", {}))
+        elif op == "decrease_budget_on":
+            ok = decrease_fluent_on(domain_ast, step.get("action_substr",""), step.get("name","budget"), int(step.get("amount",1)))
+        # ENSURE edits are for book-keeping. Don't flip OK from them
         elif op == "ensure_metric":
-            ok = ensure_metric_has_total_cost(problem_ast)
+            ensure_metric_has_total_cost(problem_ast)
         elif op == "ensure_budget":
             # two-step: declare fluent in domain + init value in problem
             ok1 = ensure_numeric_fluent(domain_ast, step.get("name","budget"))
             ok2 = ensure_init_numeric(problem_ast, step.get("name","budget"), int(step.get("init",10)))
-            ok = ok1 or ok2
-        elif op == "decrease_budget_on":
-            ok = decrease_fluent_on(domain_ast, step.get("action_substr",""), step.get("name","budget"), int(step.get("amount",1)))
+            # ok = ok1 or ok2
         else:
             log.append(f"Unknown op: {op}"); continue
         had_change = had_change or ok
@@ -77,17 +80,19 @@ def main():
         d2 = copy.deepcopy(domain_ast)
         p2 = copy.deepcopy(problem_ast)
         log = apply_recipe(d2, p2, v, verbose=True)
+        had_change = log[1]
 
-        # Write files
-        out_domain = os.path.join(args.out_dir, f"{name}_domain.pddl")
-        out_problem = os.path.join(args.out_dir, f"{name}_problem.pddl")
-        save_text(out_domain, '\n'.join(dumps(x) for x in d2))
-        save_text(out_problem, '\n'.join(dumps(x) for x in p2))
-        manifest.append({"name": name, "domain": out_domain, "problem": out_problem, "log": log})
-        print(f"Wrote {name}:")
-        for line in log: print("  ", line)
-        print("  ->", out_domain)
-        print("  ->", out_problem)
+        # Write files (only if there was change)
+        if had_change:
+            out_domain = os.path.join(args.out_dir, f"{name}_domain.pddl")
+            out_problem = os.path.join(args.out_dir, f"{name}_problem.pddl")
+            save_text(out_domain, '\n'.join(dumps(x) for x in d2))
+            save_text(out_problem, '\n'.join(dumps(x) for x in p2))
+            manifest.append({"name": name, "domain": out_domain, "problem": out_problem, "log": log})
+            print(f"Wrote {name}:")
+            for line in log: print("  ", line)
+            print("  ->", out_domain)
+            print("  ->", out_problem)
 
     # Save manifest
     man_path = os.path.join(args.out_dir, "manifest.json")
